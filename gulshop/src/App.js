07ef35d81ed359ab3b38502af378a2df019fb737
Gulzatko <Mamajanovna86@gmail.com>
2022-04-20 17:13:44 +0300

    useState

---
+++ gulshop/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import {useState} from 'react';
+import{data} from "./data";
+
+
+const App=()=> {
+  const[name, setName] = useState("Maner")
+  console.log(setName)
+
+  return(
+    <div className="App">
+      
+    </div>
+  );
+};
+
+export default App;
